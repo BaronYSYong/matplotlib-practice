abe56547f4e058453e25a1f932b0b5fc95454c0e
YS Yong <yoonseong.yong@groove-x.com>
2018-06-13 14:11:22 +0900

    update hist/hist_bar.py

--- hist/hist_bar.py
+++ hist/hist_bar.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 bins = 10
 data = np.random.randn(1000, 3)
 
+print data
+
 colors = ['blue','green', 'red']
 plt.hist(data, bins, histtype='bar', color=colors, stacked=True, fill=True)
 
